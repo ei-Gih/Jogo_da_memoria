4d999761ebba98037448b83224215fa1052fe10a
Gisele Lais <giselelais04@gmail.com>
2023-03-21 19:53:51 -0300

    primeira_fase

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_btn');
+
+function validateInput(){
+
+}
+
+const validateInput = () {
+    
+}
+
+input.addEventListener('input', validateInput);
